4423c12b64c9ece3f1d6febb243e8107f6270464
Shllokkk <shllokosan23@gmail.com>
2023-12-19 17:15:48 +0530

    adding on 19/12/23

--- xmlfileparser/XMLPreProcessor.java
+++ xmlfileparser/XMLPreProcessor.java
@@ -26,12 +26,48 @@ public class XMLPreProcessor {
                 String outputLine=convertSelfClosingTags(inputLine);
                 bw.write(outputLine+"\n");
             }
+            br.close();
+            bw.close();
         }catch(IOException e) {
             e.printStackTrace();
         }
     }
 
+    //<title>Document</title>
     public String convertSelfClosingTags(String line) {
-        return line;
+        
+        StringBuilder modifiedLine=new StringBuilder();
+        
+        int index=-1;
+        int previousIndexToCopyFrom=0;
+
+        while((index=line.indexOf('<',index+1))!=-1) {
+            int startIndex=index;
+            int closeIndex=line.indexOf('>', index+1);
+            String tag=line.substring(startIndex, closeIndex+1);
+
+            if(isSelfClosing(tag)) {
+                String tagElement=tag.substring(1, tag.length()-2);
+
+                int tagEndIndex=tagElement.indexOf(' ')==-1?tagElement.length():tagElement.indexOf(' ');
+
+                String tagName=tagElement.substring(0, tagEndIndex);
+
+                String startTag="<"+tagElement+">";
+                String endTag="</"+tagName+">";
+
+                String replaceTag=startTag+endTag;
+
+                modifiedLine.append(line.substring(previousIndexToCopyFrom,startIndex));
+                modifiedLine.append(replaceTag);
+                previousIndexToCopyFrom=closeIndex+1;
+            }
+        }
+        modifiedLine.append(line.substring(previousIndexToCopyFrom));
+        return modifiedLine.toString();
+    }
+
+    private boolean isSelfClosing(String tag) {
+        return tag.endsWith("/>");
     }
 }
